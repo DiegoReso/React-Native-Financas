14b100dcef9850acd36d2c394342386a9bebb936
Diego Reis <diegoreso@me.com>
2023-07-07 18:09:18 -0300

    create the header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 
 
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native'
+import {Feather} from '@expo/vector-icons'
 
 const statusBarHeight = StatusBar.currentHeight 
   ? StatusBar.currentHeight + 30
@@ -11,8 +11,13 @@ const statusBarHeight = StatusBar.currentHeight
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>Arouuu!</Text>
-      
+      <View style={styles.content}>
+        <Text style={styles.username}>Diego Reis</Text>
+     
+      <TouchableOpacity activeOpacity={0.5} style={styles.buttonUser}>
+        <Feather name="user" size={27} color ='white'/>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -22,5 +27,28 @@ const styles = StyleSheet.create({
   container:{
     backgroundColor: '#8000ff',
     paddingTop: statusBarHeight,
+    flexDirection:'row',
+    paddingStart: 16,
+    paddingEnd: 16,
+    paddingBottom:44,
+  },
+  content:{
+    flex:1,
+    alignItems: 'center',
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  buttonUser:{
+    width: 44,
+    height: 44,
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+  },
+  username:{
+    fontSize: 18,
+    color: '#FFF',
+    fontWeight:'bold',
   }
 })
